19724ea49b2fc7e7add16f93aacd8eefa3f44c61
Vishva <adharsh.scipy@gmail.com>
2025-10-03 14:04:52 +0530

    clinic crud

--- auth-services/routes/clinicRouter.js
+++ auth-services/routes/clinicRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { loginClinic, registerClinic } from "../controller/clinicController.js";
+import { loginClinic, registerClinic, viewAllClinics, viewClinicById, editClinic } from "../controller/clinicController.js";
 
-const clinicAuthRoutes=Router();
+const clinicAuthRoutes = Router();
 clinicAuthRoutes.route("/register").post(registerClinic);
 clinicAuthRoutes.route("/login").post(loginClinic);
+clinicAuthRoutes.route("/allclinics").get(viewAllClinics);
+clinicAuthRoutes.route("/viewClinic/:id").get(viewClinicById);
+clinicAuthRoutes.route("/editClinic/:id").put(editClinic)
 
 export default clinicAuthRoutes
