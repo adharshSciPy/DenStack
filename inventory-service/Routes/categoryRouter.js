3be2ded45bb3e023a4e1847147989a941264b8f5
Vishva <adharsh.scipy@gmail.com>
2025-10-07 14:51:37 +0530

    auth middleware inventory

--- inventory-service/Routes/categoryRouter.js
+++ inventory-service/Routes/categoryRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { createCategory, categoryDetails, deleteCategory } from "../Controller/categoryController.js"
+import { verifyAuthToken, authorizeRoles } from "../middlewares/authmiddleware.js"
+const SUPER_ADMIN = process.env.SUPERADMIN_ROLE
+
 const categoryRoute = Router()
 
-categoryRoute.route("/createCategory").post(createCategory)
-categoryRoute.route("/categoryDetails").get(categoryDetails)
-categoryRoute.route("/deleteCategory/:id").delete(deleteCategory)
+categoryRoute.post("/createCategory", verifyAuthToken, authorizeRoles(SUPER_ADMIN), createCategory)
+categoryRoute.get("/categoryDetails", verifyAuthToken, authorizeRoles(SUPER_ADMIN), categoryDetails)
+categoryRoute.delete("/deleteCategory/:id", verifyAuthToken, authorizeRoles(SUPER_ADMIN), deleteCategory)
 
 export default categoryRoute;
