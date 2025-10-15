672ae162c5062f4a238fadd4fc410e5ff1611116
Vishva <adharsh.scipy@gmail.com>
2025-10-15 16:17:26 +0530

    twilio pending

--- patient-service/routes/patientRegisterRoutes.js
+++ patient-service/routes/patientRegisterRoutes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { getAllPatients, getPatientsByClinic, getPatientWithUniqueId, patientCheck, registerPatient } from "../controller/patientRegisterController.js";
-const patientRegisterRouter=Router();
+import { getAllPatients, getPatientsByClinic, getPatientWithUniqueId, patientCheck, registerPatient, sendSMSLink, setPassword, login } from "../controller/patientRegisterController.js";
+const patientRegisterRouter = Router();
 patientRegisterRouter.route("/register/:id").post(registerPatient)//{id:clinicId}
 patientRegisterRouter.route("/single-patient").get(getPatientWithUniqueId)
 patientRegisterRouter.route("/all-patients/:id").get(getAllPatients)//{id:clinicId}
 patientRegisterRouter.route("/verify").post(patientCheck);
 patientRegisterRouter.route("/clinic-patients/:id").get(getPatientsByClinic);//id:clinicId
+
+patientRegisterRouter.post("/send-login-link", sendSMSLink);
+patientRegisterRouter.post("/set-password", setPassword);
+patientRegisterRouter.post("/patientLogin", login);
 
 
 export default patientRegisterRouter;
