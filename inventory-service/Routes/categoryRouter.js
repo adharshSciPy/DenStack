f059c8ed279cfd51702de3af784b4a2740a3157a
Vishva <adharsh.scipy@gmail.com>
2025-12-29 16:44:14 +0530

    category products

--- inventory-service/Routes/categoryRouter.js
+++ inventory-service/Routes/categoryRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createCategory, getmainCategory, getSubCategory, getAllCategory, deleteCategory } from "../Controller/categoryController.js"
+import { createCategory, getmainCategory, getSubCategory, getAllCategory, deleteCategory, categoryProducts } from "../Controller/categoryController.js"
 import { verifyAuthToken, authorizeRoles } from "../middlewares/authmiddleware.js"
 const SUPER_ADMIN = process.env.SUPERADMIN_ROLE
 const CLINIC_ROLE = process.env.CLINIC_ROLE
@@ -11,5 +11,6 @@ categoryRoute.get("/getAllCategory", verifyAuthToken, authorizeRoles(SUPER_ADMIN
 categoryRoute.get("/maincategories", verifyAuthToken, authorizeRoles(SUPER_ADMIN, CLINIC_ROLE), getmainCategory)
 categoryRoute.get("/getSubCategories/:id", verifyAuthToken, authorizeRoles(SUPER_ADMIN, CLINIC_ROLE), getSubCategory)
 categoryRoute.delete("/deleteCategory/:id", verifyAuthToken, authorizeRoles(SUPER_ADMIN, CLINIC_ROLE), deleteCategory)
+categoryRoute.get("/categoryProducts", verifyAuthToken, authorizeRoles(SUPER_ADMIN), categoryProducts)
 
 export default categoryRoute;
