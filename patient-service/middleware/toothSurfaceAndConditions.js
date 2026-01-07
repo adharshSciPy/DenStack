d8ec647cd34113854900ec6edee2e9152ff5f345
gokul-sunil <gokulsunil2015@gmail.com>
2026-01-07 23:53:55 +0530

    enum

--- patient-service/middleware/toothSurfaceAndConditions.js
+++ patient-service/middleware/toothSurfaceAndConditions.js
@@ -2,8 +2,10 @@ export const TOOTH_SURFACES = [
   'mesial', 'distal', 'occlusal', 'buccal', 'lingual', 'palatal', 'incisal', 'entire'
 ];
 
+// In your toothSurfaceAndConditions.js file, update TOOTH_CONDITIONS:
 export const TOOTH_CONDITIONS = [
   'caries', 'fractured', 'sensitive', 'impacted', 'missing',
   'discolored', 'mobile', 'periapical-lesion',
-  'periodontal-pocket', 'calculus', 'plaque', 'gingivitis', 'pericoronitis'
+  'periodontal-pocket', 'calculus', 'plaque', 'gingivitis', 'pericoronitis',
+  'Caries', 'Periapical Lesion', 'Sensitive', 'Filling', 'Discolored' // Add Discolored here
 ];
